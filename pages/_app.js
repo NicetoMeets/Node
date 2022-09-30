6b9f2b55e71fa4f9443c4950669856650b649c8c
Nice <puma1800@naver.com>
2022-09-30 22:51:06 +0900

    bird

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import PropTypes from 'prop-types';
+
+const NodeBird = ({ Component }) =>{
+    return (
+        <>
+        <Component></Component>
+        </>
+    )
+};
+
+NodeBird.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+}
+
+
+export default NodeBird;
